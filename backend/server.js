1f81d82c3793480d6d11c1fa06115121f8155a47
Priyanshu Solanki <Priyanshusolankii@outlook.com>
2025-12-17 22:32:38 +0530

    update  the env

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL || '*',
+    credentials: true
+}));
 
 app.get('/', (req, res) => {
     res.send('API is running...');
